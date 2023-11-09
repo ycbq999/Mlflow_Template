83381599edb03d8b912d21e0d03b79e5af4aa178
STC <ycbq999@hotmail.com>
2023-11-09 03:24:10 +0000

    a

--- command.sh
+++ command.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
-
+##### Part1 setting up environment
 # create conda mlflow environment
-conda create --name mlfow python=3.8
+conda create --name mlfow python=3.10.4
 # activate mlflow environment
 conda activate mlflow 
 
@@ -12,4 +12,19 @@ conda activate mlflow
 # conda env export --name mlflow > conda_env.yml
 
 # Since you added the pip installs directly to the file, you didn't really install them, so go ahead and run the following command to ensure everything is working:
-conda env update --file conda_env.yml --prune
+# conda env update --file conda_env.yml --prune
+
+
+
+### part 2 mlflow tutorial  
+
+# Step 1: Install MLflow from PyPI
+
+# pip install mlflow
+
+# Step 2: Launch the MLflow Tracking Server
+
+# mlflow server --host 127.0.0.1 --port 8080
+
+# Then you can run logging-first-model.ipynb and read the tutorial
+
